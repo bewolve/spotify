2c5f8906c0c81816387ec0bad1cf5ffe251b1b3a
bewolve <bewolve@gmalil.com>
2024-03-25 10:17:45 +0400

    added icon to followers and made everything responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { PopularSongs } from "@/components/PopularSongs";
 import { authOptions } from "@/lib/auth";
 import { User } from "@/types";
 import { getServerSession } from "next-auth";
-import Image from "next/image";
+import { BsPeople } from "react-icons/bs";
 
 export default async function Home() {
   const user: User | null = await getServerSession(authOptions);
@@ -20,18 +20,18 @@ export default async function Home() {
 
   return (
     <section className="container flex flex-1 flex-col py-8">
-      <div className="wrapper grid flex-1 grid-rows-[1_1fr] gap-4 md:grid-cols-4">
+      <div className="wrapper grid flex-1 grid-cols-[1fr] grid-rows-[1_1fr_1fr] gap-4 md:grid-cols-4 md:grid-rows-[1_1fr]">
         <img
           src={data.images[0].url}
           alt="sadas"
           className="col-span-2 h-full object-cover"
         />
 
-        <div className="headers hidden space-y-4 text-secondary md:col-span-2 md:flex md:flex-col md:justify-center">
+        <div className="headers z-50 space-y-4 text-secondary md:col-span-2 md:flex md:flex-col md:justify-center">
           <h1 className="text-5xl">
             Follow{" "}
             <a
-              className="font-bold text-primary underline underline-offset-4"
+              className="font-bold text-primary underline decoration-secondary underline-offset-4"
               href={data.external_urls.spotify}
               target="_blank"
             >
@@ -46,12 +46,13 @@ export default async function Home() {
             odio.
           </p>
 
-          <p className="followers">
+          <p className="followers flex items-center gap-2">
+            <BsPeople />
             {formatter.format(data.followers.total)} Followers
           </p>
         </div>
 
-        <div className="popular row-start-2 space-y-4 self-center overflow-x-scroll scrollbar-hide md:col-span-4">
+        <div className="popular row-start-3 space-y-4 self-center overflow-x-scroll scrollbar-hide md:col-span-4">
           <h1 className="text-xl font-semibold text-secondary">
             Popular Track You May Like
           </h1>
